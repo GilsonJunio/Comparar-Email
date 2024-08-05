b45640bddd1fde49de197727a7fec579a728d2eb
Gilson Junio Sales Andrade <gilsonjunio@quimicojapones.com.br>
2024-08-04 22:43:00 -0300

    função de comparar emails somente

--- script.js
+++ script.js
@@ -1,61 +1,32 @@
-/*
-A função buscar_Dados() requisita um arquivo json no meu repositório;
-Após os devidos tratamentos, os dados JSON são atribuídos à variável usuarios;
-A função buscar_Dados() retorna a variável usuarios.
-
-Quando atribuir buscar_Dados() à uma variável "teste", ela retornará uma promise com dois protótipos: "<status>" e "<value>";
-O protótipo value conterá um objeto com um array de múltiplos objetos contendo dados de  usuários em formato JSON.
-
-Para acessar o array retornado pelo protótipo, é necessário aplicar o método then() na variável "teste"; 
-Este método acessará os dados quando o status do retorno for fulfilled, nesse caso, o protótipo "<status>" está fulfilled;
-Assim, uma nova varíavel que receba 'teste.then()' será capaz de acessar o array.
-
-O método ".then" tem sintaxe: ".then(parametro => {})", assim o array de teste se tornará disponível através de "parametro";
-aplique ".then(parametro => {console.log(parametro)})" para averiguar o array no console.
-
-*/
-
-
-let email_Repassado = "joao@exemplaaaaao.com"
+let email_Repassado = document.getElementsByClassName('email')[0].value
+let botao_entrar = document.getElementsByClassName('botao')[0]
 
 async function buscar_Dados(){
 	//busque seus usuarios abaixo
 	const requisicao = await fetch('https://raw.githubusercontent.com/GilsonJunio/Usuarios-JSON/main/usuarios.json')
-		//console.log(requisicao)
 	const tratar = await requisicao.json()
-		//console.log(tratar)
-
-	usuarios = tratar.usuarios
-		//console.log(usuarios)
-
+	let usuarios = tratar.usuarios
 	return usuarios
 }
-function verificar_Usuarios(usuarios){
-	//criar usuários
+
+function verificar_Usuarios(existe_Email_Igual){
 	let dados = buscar_Dados()
-	usuarios = dados.then((usuarios) => {			
-			console.log(usuarios)
+	let usuarios = dados.then((usuarios) => {			
 			let procurar_Email_Igual = usuarios.find(usuarios => {
-				console.log(usuarios.email === email_Repassado);
-
-				let existe_Email_Igual = usuarios.email === email_Repassado;
-					console.log(existe_Email_Igual)
-
-				if(existe_Email_Igual === true){
-					alert('O Email já existe!')
-					return true
-				}
-				alert('O Email não existe!')
-				return false
-		})	
+			let existe_Email_Igual = usuarios.email === email_Repassado;
+			return existe_Email_Igual
+		}) 	
+		if(procurar_Email_Igual){
+			alert('Este email já foi cadastrado!')
+			procurar_Email_Igual = true
+			return procurar_Email_Igual
+		}
+		else if(procurar_Email_Igual === undefined){
+			return false
+		}
+		else{
+			alert('Email cadastrado com sucesso!')
+		}
 	})
-
+	return usuarios
 }
-if(verificar_Usuarios() === false){
-	alert('taaaaaaaa')
-}
-
-/*
-Chame a função comparar_Email('email') no console
-Coloque o email para comparar com o banco de dados	
-*/
